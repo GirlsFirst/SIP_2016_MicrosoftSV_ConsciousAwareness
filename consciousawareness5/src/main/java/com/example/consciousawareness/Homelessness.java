821da3cee9ddfde4791770957149950dd9adb7ec
haleytran01 <haleytran01@gmail.com>
2016-08-11 11:22:25 -0700

    init

---
+++ consciousawareness5/src/main/java/com/example/consciousawareness/Homelessness.java
@@ -0,0 +1,13 @@
+package com.example.consciousawareness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Homelessness extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homelessness);
+    }
+}
---
+++ gun control/src/main/java/com/example/consciousawareness/GunControl.java
@@ -0,0 +1,13 @@
+package com.example.consciousawareness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GunControl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gun_control);
+    }
+}
